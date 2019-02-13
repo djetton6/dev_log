3eca151ca7d35ae209a5bd7f7396429a298c8ee1
Desmend Jetton <djetton@iit.edu>
2019-02-13 05:46:15 -0600

    created data schema, added new simple form and created new success msg.

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -1,4 +1,33 @@
 class DevelopersController < ApplicationController
+    before_action :find_developer, only: [:show, :edit, :update, :destroy]
+    
     def index
+    end
+
+    def show
+    end
+
+    def new
+        @developer = Developer.new
+    end
+
+    def create
+        @developer = Developer.new(developer_params)
+
+        if @developer.save
+            redirect_to @developer, notice:"Awesome! You've met a new developer. Don't forget to build something dope together."
+        else
+            render 'new'
+        end
+    end 
+
+    private
+
+    def developer_params
+        params.require(:developer).permit(:title, :description)
+    end
+
+    def find_developer
+        @developer = Developer.find(params[:id])
     end
 end
---
+++ db/migrate/20190213084432_create_developers.rb
@@ -0,0 +1,11 @@
+class CreateDevelopers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :developers do |t|
+      t.string :title
+      t.text :description
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
